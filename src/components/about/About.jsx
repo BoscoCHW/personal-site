3661faa7e27a15d8b47c610c16df3a48ecb3894b
Bosco_Acer <bosco.chw@gmail.com>
2022-03-05 19:59:58 -0800

    change pdf link

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
         </div>
         
         <div>
-          <button><a href="./assets/images/bosco-chan-resume.pdf" download>Download my resume!</a></button>
+          <button><a href="https://drive.google.com/file/d/1bQ67Ve2j88Y6G-xjYbj8xhQBjy5zdp0A/view" download>Download my resume!</a></button>
         </div>
       </div>
     </div>
